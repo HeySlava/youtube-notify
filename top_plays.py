04732c0d651cf86ffd2810c1f72c108ff4cc7b05
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-12-01 00:48:33 +0300

    Move from pytube to ffmpeg

--- top_plays.py
+++ top_plays.py
@@ -1,16 +1,28 @@
 import json
 import os
 import pathlib
+import re
+import subprocess
+from typing import List
 from typing import NamedTuple
-from typing import Sequence
+from typing import Pattern
+from typing import Set
 
-import pytube
 import requests
+
+
+SEP = '____'
 
 
 class Playlist(NamedTuple):
     url: str
     tag: str
+    pattern: Pattern[str]
+
+
+class Video(NamedTuple):
+    url: str
+    title: str
 
 
 class History:
@@ -25,24 +37,27 @@ class History:
     def _init(self) -> None:
         if not self.history.parent.exists():
             self.history.parent.mkdir()
-        if not self.history.exists():
-            with open(self.history, 'w') as f:
-                json.dump([], f)
 
-    def update_history(self, videos: Sequence[str]) -> None:
+    def update_history(self, videos: List[str]) -> None:
         with open(self.history, 'w') as f:
-            json.dump(list(videos), f)
+            json.dump(videos, f)
 
     @property
     def old_videos(self) -> list[str]:
+        if (
+                not self.history.exists() or
+                not self.history.read_text()
+        ):
+            return []
         with open(self.history) as f:
             videos = json.load(f)
         return videos
 
 
 playlist = Playlist(
-        url='https://www.youtube.com/playlist?list=PLlVlyGVtvuVnHgxDVDxbRuObq6XSKiCxZ',  # noqa: E501
+        url='https://www.youtube.com/@NBA/videos',
         tag='NBA',
+        pattern=re.compile(r"NBA's Top \d+ Plays Of The Night"),
     )
 
 
@@ -69,18 +84,50 @@ def notify(urls: set[tuple[str, str]]) -> None:
         _notify(url, tag)
 
 
+def _parse_output(
+        pattern: Pattern[str],
+        output: str,
+) -> Set[Video]:
+    result = set()
+    base_url = 'https://www.youtube.com/watch?v={video_id}'
+    for line in output.splitlines():
+        if pattern.search(line):
+            title, _, rest = line.strip('"').partition(SEP)
+            _, _, video_id = rest.rpartition('=')
+            url = base_url.format(video_id=video_id)
+            result.add(Video(title=title, url=url))
+    return result
+
+
+def _get_new_videos(playlist: Playlist) -> Set[Video]:
+    cmd = (
+            'yt-dlp', '-I0:30', '-s', '--flat-playlist', '-o',
+            f'"%(title)s {SEP} %(original_url)s"', '--print', 'filename',
+            playlist.url,
+
+        )
+    p = subprocess.run(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
+    return _parse_output(pattern=playlist.pattern, output=p.stdout)
+
+
 def main() -> int:
-    p = pytube.Playlist(playlist.url)
     history = History(
             pathlib.Path(f'./storage/{playlist.tag}') / 'history.json'
         )
-    new_videos = {
+    last_videos = _get_new_videos(playlist)
+    new_video_tags = {
             (url, playlist.tag) for
             url in
-            set(p) - set(history.old_videos)
+            {v.url for v in last_videos} - set(history.old_videos)
         }
-    notify(new_videos)
-    history.update_history(videos=p)
+    notify(new_video_tags)
+    all_videos = set(history.old_videos).union({v.url for v in last_videos})
+    history.update_history(videos=list(all_videos))
     return 0
 
 
